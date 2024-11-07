4020ef4c809951988b9259de172489160ce8c1d2
dilipsagar207 <143000535+dilipsagar207@users.noreply.github.com>
2024-11-07 23:41:15 +0530

    changed url in ordercontroller.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -60,7 +60,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // };
 
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://food-delivery-frontend-7mx9.onrender.com";
 
     try {
         const newOrder = new orderModel({
